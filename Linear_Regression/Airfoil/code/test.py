30dda35ab047221ac7389a70f7ce7d0a06a66c9a
SudhaDevi17 <joonnypapa105@gmail.com>
2020-03-23 10:57:31 +0530

    travis test

--- Linear_Regression/Airfoil/code/test.py
+++ Linear_Regression/Airfoil/code/test.py
@@ -4,5 +4,5 @@ PATH = 'Linear_Regression/Airfoil/airfoil.csv'
 
 #test
 class TestAlgorithms(TestCase):
-    from Linear_Regression.Airfoil.code.Airfoil import train
+    from  Airfoil import train
     train(PATH)
